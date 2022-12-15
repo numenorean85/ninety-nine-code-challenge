f2786020a0d85cf706d457120310f77ae8c9d45b
David Díaz García <david.diaz@in-tact.fr>
2022-12-15 11:49:13 +0100

    chore. Added docker compose file, swagger and modelMapper dependencies and added first properties files

--- src/main/kotlin/com/ninetynine/codechallenge/NinetyNineApplication.kt
+++ src/main/kotlin/com/ninetynine/codechallenge/NinetyNineApplication.kt
@@ -1,9 +1,15 @@
 package com.ninetynine.codechallenge
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableJpaRepositories
+@ConfigurationPropertiesScan("com.ninetynine.codechallenge.config.properties")
+@EnableScheduling
 class NinetyNineApplication
 
 fun main(args: Array<String>) {
